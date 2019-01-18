b2a62c79456afe5a97e7c1cb06f76c934f5347fc
Pallavi Bhagat <akumari@zeomega.com>
2019-01-18 12:45:38 +0530

    Add ability to make posts from the administrator interface.

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField()
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = now()
+        self.save()
+
+    def __str__(self):
+        return self.title
